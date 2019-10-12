0818dcb9f4419720620659abaff7436fdfc14791
dOrgJelli <jelli@dorg.tech>
2019-10-12 16:04:09 +0300

    config-overrides

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,3 @@
 module.exports = function override(config, env) {
-  // TODO: this is a hack to get around the issue in
-  // src/lib/dependencies/arc/migrate.ts
-  // implement a real fix later
-  config.plugins.forEach(plugin => {
-    if (plugin.ignoreLintWarnings !== undefined) {
-      plugin.ignoreLintWarnings = true;
-    }
-  });
   return config;
 };
